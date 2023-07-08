5e82fd9e848d330fb8871a3901e62bdd4a7a03f2
abcdefghijorngarbosaxyz <jornblaedelgarbosa@proton.me>
2023-07-09 04:58:48 +0800

    Made the App Run, but...
    Added temporary index.html to trick tauri builder that theres a frontend, but there isn't yet.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
 #[tokio::main]
 async fn main() {
-  println!("Hello World with Tokio");
+  let context = tauri::generate_context!();
+
+  let app_logger = tauri_plugin_log::Builder::default().targets([
+    tauri_plugin_log::LogTarget::LogDir,
+    tauri_plugin_log::LogTarget::Stdout,
+    tauri_plugin_log::LogTarget::Stderr,
+    tauri_plugin_log::LogTarget::Webview,
+  ]);
+
+  let app_builder = tauri::Builder::default().plugin(app_logger.build());
+
+  if let Err(error) = app_builder.run(context) {
+    log::error!("Error while running generating context: {}", error);
+  } else {
+    log::info!("Context generated.");
+  }
 }
